988f7dfaf2fe5a7d438a5ec9e1f05e9763ba7f71
sopendix <supremewit@gmail.com>
2025-07-19 17:52:04 +0900

    Update sheet.js

--- netlify/functions/sheet.js
+++ netlify/functions/sheet.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 exports.handler = async function (event, context) {
   const SHEET_ID = '1tmUnbXHENlO6TYXN1bmzCDRhtG1d2qENfgDQk7KdX9s';
   const API_KEY = process.env.GOOGLE_SHEET_API_KEY;
-
+  const origin = event.headers.origin;
   const ranges = ['Allstar!A2:H', 'Uhlmann!A2:H'];
 
   // 각 시트를 fetch 요청으로 변환
@@ -17,14 +17,26 @@ exports.handler = async function (event, context) {
   
   const combinedRows = results.flatMap(result => result.values || []);
 
+  const allowedOrigins = [
+    'https://fencingshop.kr',
+    'https://sopendix.github.io'
+    ]
+
+  const headers = { 
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+   };
+
+  if (allowedOrigins.includes(origin)) {
+    headers['Access-Control-Allow-Origin'] = origin;
+  } else {
+    headers['Access-Control-Allow-Origin'] = null;
+  }
 
 
   return {
     statusCode: 200,
-    headers: { 
-      'Access-Control-Allow-Origin': 'https://sopendix.github.io',
-      'Access-Control-Allow-Headers': 'Content-Type'
-     },
-    body: JSON.stringify({ values: combinedRows })
+    headers,
+    body: JSON.stringify({ values: combinedRows }),
   };
 };
